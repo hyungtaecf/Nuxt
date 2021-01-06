ed109649c05492a5fbe4ea4b05790e3f7bdfc6cb
Hyung Tae C. Figur <hyu_03@hotmail.com>
2021-01-06 19:27:03 -0400

    First commit

---
+++ layouts/default.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container">
+    <AppHeader />
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+import { AppHeader } from "../components/AppHeader";
+export default {
+  components: {
+    AppHeader,
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  font-style: 1rem;
+  line-height: 1.6;
+  background: #f4f4f4;
+}
+
+a {
+  color: #666;
+  text-decoration: none;
+}
+
+ul {
+  list-style: none;
+}
+
+.container {
+  min-width: 800px;
+  margin: 2rem auto;
+  overflow: hidden;
+  padding: 1rem 2rem;
+  background: #fff;
+}
+</style>
